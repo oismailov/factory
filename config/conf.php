f5f60aa4d1bd58f256ce7fb3ab01cbd34ef91e55
Oleh Ismailov <oleh@vivino.com>
2015-11-10 01:41:16 +0200

    typo mistake

--- config/conf.php
+++ config/conf.php
@@ -4,7 +4,7 @@ define('ROOT_DIR', realpath(__DIR__ . '/../') . "/");
 
 function __autoload($name)
 {
-    $file = $file = ROOT_DIR . "inc/$name.php";
+    $file = ROOT_DIR . "inc/$name.php";
 
     if (file_exists($file)) {
         require_once $file;
